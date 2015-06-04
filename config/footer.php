00ca62bea0554b749973d7282469b6fd23efb076
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-04 14:42:37 +0300

    PSR

--- config/footer.php
+++ config/footer.php
@@ -5,13 +5,13 @@ $app['bugmanager'] = new Bugmanager($app['config']['db']['dsn'], $app['config'][
 
 try {
     $app['bugmanager']->connect();
-} catch (Exception $exc){
+} catch (Exception $exc) {
     Response::responseWithError($exc->getMessage());
 }
 
 $i18n = $app['i18n'] = $app['i18n'][$app['locale']];
 
-
-if(!empty($_REQUEST['action']) && isset($app['controllers'][$_REQUEST['action']])):
+if (!empty($_REQUEST['action']) && isset($app['controllers'][$_REQUEST['action']])):
     $app['controllers'][$_REQUEST['action']]($app, $_REQUEST);
+    die();
 endif;
--- config/header.php
+++ config/header.php
@@ -1,27 +1,23 @@
 <?php
 
-$app = array();
+$app = [];
 
-$app['config'] = array(
-    'db' => array(
-        'dsn'       => 'mysql:dbname=bugmanager;host=localhost',
-        'user'      => 'bugmanager',
-        'password'  => ''
-    ),
-    'url'           => $_SERVER['PHP_SELF'],
-    'debug'         => false,
-    'issue_types'   => array(
-        array('type' => 'bug'),
-        array('type' => 'feature')
-    )
-);
+$app['config'] = [
+    'db' => [
+        'dsn' => 'mysql:dbname=bugmanager;host=localhost',
+        'user' => 'bugmanager',
+        'password' => '',
+    ],
+    'url' => $_SERVER['PHP_SELF'],
+    'debug' => false,
+    'issue_types' => [
+        ['type' => 'bug'],
+        ['type' => 'feature']
+    ]
+];
 
-
-
-if($app['config']['debug']):
+if ($app['config']['debug']):
     error_reporting(E_ALL);
-    ini_set('display_errors', 1);
 endif;
-
 
 $app['locale'] = 'en';
