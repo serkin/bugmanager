b415e193d21862973efa8fdb8f8791a1c85afb82
Serkin Alexander <serkin.alexander@gmail.com>
2015-06-11 13:06:25 +0300

    Add basic functional test

---
+++ tests/functional/test.php
@@ -0,0 +1,54 @@
+<?php
+
+class WebTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://localhost:4000/bugmanager.php');
+    }
+
+    public function testTitle()
+    {
+        $this->open('http://localhost:4000/bugmanager.php');
+        $this->assertTitle('Bugmanager');
+        $this->waitForNotOrdered();
+    }
+}
+/*
+    public function testContact()
+    {
+        $this->open('site/contact');
+        $this->assertTextPresent('Contact Us');
+        $this->assertElementPresent('name=ContactForm[name]');
+        $this->type('name=ContactForm[name]','tester');
+        $this->type('name=ContactForm[email]','tester@example.com');
+        $this->type('name=ContactForm[subject]','test subject');
+        $this->clickAndWait("//input[@value='Submit']");
+        $this->assertTextPresent('Body cannot be blank.');
+    }
+    public function testLoginLogout()
+    {
+        $this->open('');
+        // ensure the user is logged out
+        if($this->isTextPresent('Logout'))
+            $this->clickAndWait('link=Logout');
+        // test login process, including validation
+        $this->clickAndWait('link=Login');
+        $this->assertElementPresent('name=LoginForm[username]');
+        $this->type('name=LoginForm[username]','demo');
+        $this->clickAndWait("//input[@value='Login']");
+        $this->assertTextPresent('Password cannot be blank.');
+        $this->type('name=LoginForm[password]','demo');
+        $this->clickAndWait("//input[@value='Login']");
+        $this->assertTextNotPresent('Password cannot be blank.');
+        $this->assertTextPresent('Logout');
+        // test logout process
+        $this->assertTextNotPresent('Login');
+        $this->clickAndWait('link=Logout');
+        $this->assertTextPresent('Login');
+    }
+}
+*/
+
+
